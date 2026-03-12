680763725bb56a26891296e5d643a5d6d6cf992f
Isabela Helena <isabela_linhares@estudante.sesisenai.org.br>
2026-03-12 16:01:39 -0300

    Create arquivo.js

---
+++ 12-03-2026/arquivo.js
@@ -0,0 +1,42 @@
+//exercício1
+//let num1 = 2
+//let num2 = 2
+
+//console.log(num1+num2)
+
+//exercício2
+
+// let num1 = 5
+// let num2 = 3
+
+// console.log(num1-num2)
+
+//exercício3
+
+// let num1 = 5 
+// let num2 = 10 
+
+// console.log(num1*num2)
+
+//exercício4
+
+// let num1 = 10
+// let num2 = 2
+
+// console.log(10/2)
+
+//exercício5
+
+// let num1 = 10
+// let num2 = 2
+
+// console.log(10%2)
+
+//exercício6
+
+//  let num1 = 6
+//  let num2 = 8 
+
+//  console.log(6**8)
+
+//exercício6
